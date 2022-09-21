1dfdb713c891444ef58df3d3ac69331196552baf
JLC0126 <jcastillor@unah.hn>
2022-09-21 15:33:20 -0600

    Miembro de datos edad
    Se agrego el miembro de datos edad, se modifico el constructor y se agregaron las funciones miembro set_edad y get_edad.

--- Estudiantes.cpp
+++ Estudiantes.cpp
@@ -9,9 +9,10 @@ using std::string;
 class Estudiante{
 
     public:
-        Estudiante(string mi_nombre, string mi_id_estudiante){ // constructor
+        Estudiante(string mi_nombre, int mi_edad, string mi_id_estudiante){ // constructor
             nombre = mi_nombre;
             id_estudiante = mi_id_estudiante;
+            edad=mi_edad;
         }
 
         //funciones miembros
@@ -22,19 +23,27 @@ class Estudiante{
         string get_nombre(){
         	return nombre;
 		}
+		
+		int get_edad(){
+			return edad;
+		}
+		
+		void set_edad(int nueva_edad){
+			edad=nueva_edad;
+		}
 
 
     private:
         string nombre, id_estudiante, grado; //miembros de datos
         string generro;
-        int num_clases;
+        int num_clases, edad;
 };
 
 int main(){
 
-    Estudiante est1("Ronaldo","123-456-789"); //Objeto: "Una variable"
-    Estudiante est2("Estefani","233-413");
-    Estudiante est3("Carlo","42312-121312");
+    Estudiante est1("Ronaldo",38,"123-456-789"); //Objeto: "Una variable"
+    Estudiante est2("Estefani",18,"233-413");
+    Estudiante est3("Carlo",25,"42312-121312");
 
     int variable =5;
 
@@ -46,6 +55,12 @@ int main(){
     cout<<"El id del estuniante 1 es: "<<est1.get_id()<<endl;
     cout<<"otro id: "<<est3.get_id()<<endl;
 	cout<<"El nombre del estudiante 1 es: "<<est1.get_nombre()<<" y su id es: "<<est1.get_id()<<endl;
+	
+	cout<<"La edad de "<<est1.get_nombre()<<" es :"<<est1.get_edad()<<endl;
+	est1.set_edad(23);
+	cout<<"La edad biologica de "<<est1.get_nombre()<<" es: "<<est1.get_edad()<<endl;
     return 0;
 }
+
+//Tarea. Crear un miembro de datos llamafdo edad y crear funciones set y get para el miembro de datos 
 
